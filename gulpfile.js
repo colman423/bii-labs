1920c089b752010e36e7ab349cde54790fa7a1ff
colman423 <colman0423@gmail.com>
2020-04-13 16:23:40 +0800

    使用gulp-inject來inject header, footer等partial

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const rename = require("gulp-rename");
 const open = require('gulp-open');
 const connect = require('gulp-connect');
 const connectRewrite = require('connect-modrewrite');
+const inject = require('gulp-inject');
 
 sass.compiler = require('node-sass');
 
@@ -33,12 +34,23 @@ gulp.task('js', function () {
 });
 
 gulp.task('html', function () {
-    return gulp.src('src/**/*.html').pipe(connect.reload());
+    return gulp.src('src/**/*.html')
+        .pipe(inject(gulp.src(['./src/partials/*.ejs']), {
+            starttag: '<!-- inject:{{path}} -->',
+            relative: true,
+            removeTags: true,
+            transform: function (filePath, file) {
+                // return file contents as string
+                return file.contents.toString('utf8')
+            }
+        }))
+        .pipe(gulp.dest('./dist'))
+        .pipe(connect.reload());
 });
 
 gulp.task('webserver', function () {
     const server = connect.server({
-        root: ['dist', 'src', 'static'],
+        root: ['dist', 'static'],
         host: '0.0.0.0', //host設定'0.0.0.0'，就可以用內網檢視
         port: 3000, //設定一個沒在使用的port
         livereload: true, //auto refresh
@@ -51,18 +63,18 @@ gulp.task('webserver', function () {
         },
     })
     return gulp.src('./')
-        .pipe(open({
-            uri: 'http://localhost:' + server.port
-        }));
+    // .pipe(open({
+    //     uri: 'http://localhost:' + server.port
+    // }));
 });
 
 gulp.task('watch', function () {
     gulp.watch('sass/**/*.scss', gulp.series('sass')); //監聽路徑，以及檔案變更後所執行的任務
     gulp.watch('js/**/*.js', gulp.series('js')); //監聽路徑，以及檔案變更後所執行的任務
-    gulp.watch('src/**/*.html', gulp.series('html'));
+    gulp.watch(['src/**/*.html', 'src/**/*.ejs'], gulp.series('html'));
 });
 
-gulp.task('default', gulp.series('clean', 'sass', 'js', gulp.parallel('webserver', 'watch'), function (done) {
+gulp.task('default', gulp.series('clean', 'sass', 'js', 'html', gulp.parallel('webserver', 'watch'), function (done) {
     done();
 }));
 
@@ -72,3 +84,10 @@ gulp.task('build', gulp.series(
     () => gulp.src('./src/**/*').pipe(gulp.dest('./deploy/')),
     () => gulp.src('./static/**/*').pipe(gulp.dest('./deploy/')),
 ));
+
+process.on('SIGINT', function () {
+    console.log("SIGINTSIGINTSIGINTSIGINT")
+    // gulp.watch.close();
+    process.exit();
+});
+//   exports.default = promiseTask;
