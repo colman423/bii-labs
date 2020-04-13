b9a8eb4e7cf3ba3ed3023c7d71a3f4aa84f0221a
colman423 <colman0423@gmail.com>
2020-04-13 17:26:09 +0800

    改變gulp deploy的寫法

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,12 @@ const open = require('gulp-open');
 const connect = require('gulp-connect');
 const connectRewrite = require('connect-modrewrite');
 const inject = require('gulp-inject');
+const cleanCSS = require('gulp-clean-css')
+const htmlmin = require('gulp-htmlmin');
 
 sass.compiler = require('node-sass');
+
+// ----- gulp dev start ----- //
 
 gulp.task('clean', function () {
     return gulp.src('./dist/', { read: false, allowEmpty: true })
@@ -16,7 +20,7 @@ gulp.task('clean', function () {
 });
 
 gulp.task('sass', function () {
-    return gulp.src('sass/*.scss')
+    return gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'))
         .pipe(connect.reload());
@@ -63,9 +67,9 @@ gulp.task('webserver', function () {
         },
     })
     return gulp.src('./')
-    .pipe(open({
-        uri: 'http://localhost:' + server.port
-    }));
+        .pipe(open({
+            uri: 'http://localhost:' + server.port
+        }));
 });
 
 gulp.task('watch', function () {
@@ -78,8 +82,52 @@ gulp.task('default', gulp.series('clean', 'sass', 'js', 'html', gulp.parallel('w
     done();
 }));
 
-gulp.task('build', gulp.series(
-    () => gulp.src('./deploy/', { read: false, allowEmpty: true }).pipe(clean({ force: true })),
-    () => gulp.src('./dist/**/*').pipe(gulp.dest('./deploy/')),
-    () => gulp.src('./static/**/*').pipe(gulp.dest('./deploy/')),
-));
+// ----- gulp dev end ----- //
+
+
+// ----- gulp deploy start ----- //
+
+gulp.task('deploy-clean', function () {
+    return gulp.src('./deploy/', { read: false, allowEmpty: true })
+        .pipe(clean({ force: true }));
+});
+
+gulp.task('deploy-sass', function () {
+    return gulp.src(['sass/**/*.scss', '!sass/vars.scss'])       // exclude var.scss fail
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({}))
+        .pipe(gulp.dest('./deploy/css'))
+});
+
+gulp.task('deploy-js', function () {
+    return gulp.src('js/**/*.js')
+        .pipe(uglify())
+        .pipe(rename(function (path) {
+            path.basename += ".min";
+            path.extname = ".js";
+        }))
+        .pipe(gulp.dest('./deploy/js'))
+});
+
+gulp.task('deploy-html', function () {
+    return gulp.src('src/**/*.html')
+        .pipe(inject(gulp.src(['./src/partials/*.ejs']), {
+            starttag: '<!-- inject:{{path}} -->',
+            relative: true,
+            removeTags: true,
+            transform: function (filePath, file) {
+                // return file contents as string
+                return file.contents.toString('utf8')
+            }
+        }))
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./deploy'))
+});
+
+gulp.task('deploy-static', function () {
+    return gulp.src('./static/**/*').pipe(gulp.dest('./deploy/'))
+})
+
+gulp.task('build', gulp.series('deploy-clean', 'deploy-sass', 'deploy-js', 'deploy-html', 'deploy-static'));
+
+// ----- gulp deploy end ----- //
