39997d021cc14feb5723bcd1bf75ffebd312029a
colman423 <colman0423@gmail.com>
2020-05-11 03:05:04 +0800

    將background-image改成image; 將parallex包成class

--- js/cases.js
+++ js/cases.js
@@ -1,11 +1,4 @@
-$(function() {
-    // var _p = Parallex($('.cases-img'));
-    Parallex.add($('.cases-img'))
-    Parallex.init();
-    // console.log("-p", _p)
-// 
-    // _p.elements.push($('.cases-img'))
-    // .elements.push($('.cases-img'))
-    // Parallex.elements.push($('.cases-img'))
-    // _p.init();
+$(function () {
+
+    $('.cases-img').parallex()
 })
--- js/vendor/parallex.js
+++ js/vendor/parallex.js
@@ -1,76 +1,55 @@
-
-var Parallex = {
-    elements: [],
-    add: function ($selectors) {
-        $selectors.each(function (idx, ele) {
-            Parallex.elements.push($(ele))
-        })
-    },
-    init: function () {
-        console.log("ParallexInit this.ele", Parallex.elements)
-        var elements = Parallex.elements;
-
-        for (var i = 0; i < elements.length; i++) {
-            $ele = elements[i]
-            $ele.css('background-size', ($ele.width()) + 'px ');
-            $ele.css('background-attachment', 'scroll');
-            $ele.css('background-position-x', 'left');
-        }
-
-        $(window).scroll(function () {
-            var windowScrollTop = $(window).scrollTop();
-            // console.log("elements.length", elements.length)
-            for (var i = 0; i < elements.length; i++) {
-                // console.log(elements[i].attr('id'), i)
-                parallex(elements[i], windowScrollTop)
-            }
-        });
-
-        function parallex($ele, windowScrollTop) {
-            var elePosY = $ele.offset().top;
-            var eleHeight = $ele.height();
-            var imgHeight = $ele.width()*942/990;
-            console.log("imgHeight", imgHeight)
-
-            if (windowScrollTop < elePosY) {
-                console.log("not into yet")
-                $ele.css('background-attachment', 'scroll');
-                $ele.css('background-position-x', 'left');
-                $ele.css('background-position-y', 'top');
-            }
-            else if (windowScrollTop > elePosY + eleHeight - imgHeight) {
-                console.log("scroll complete")
-                $ele.css('background-attachment', 'scroll');
-                $ele.css('background-position-x', 'left');
-                $ele.css('background-position-y', 'bottom');
-
-            }
-            else {
-                console.log("change to fixed")
-                $ele.css('background-attachment', 'fixed');
-                $ele.css('background-position-x', ($ele.offset().left) + 'px ');
-                $ele.css('background-position-y', 'top');
-            }
-
-        }
-    }
-
+function ParallexElement($ele) {
+    this.$ele = $ele;
+    this.elePosY = $ele.offset().top;
+    this.imgHeight = $ele.width() * 942 / 990;      // todo
+    this.eleHeight = $ele.height();
+    this.eleWidth = $ele.innerWidth();
 }
 
-// Parallex.prototype.elements = []
+function Parallex() {
+    this.elements = []
+    $(window).scroll(this.onScroll.bind(this))
+}
+Parallex.prototype.onScroll = function () {
+    console.log("onScroll!", this)
+    var windowScrollTop = $(window).scrollTop();
+    for (var i = 0; i < this.elements.length; i++) {
+        this.onScrollEle(this.elements[i], windowScrollTop)
+    }
+}
+Parallex.prototype.onScrollEle = function (parallexElement, windowScrollTop) {
 
-// Parallex.prototype.init = function () {
+    var $ele = parallexElement.$ele;
+    var elePosY = parallexElement.elePosY;
+    var imgHeight = parallexElement.imgHeight;
+    var eleHeight = parallexElement.eleHeight;
+    var eleWidth = parallexElement.eleWidth;
+    console.log("onScrollEle", "windowScrollTop", windowScrollTop, "elePosY", elePosY, "eleHeight", eleHeight, "imgHeight", imgHeight)
 
-// }
+    if (windowScrollTop < elePosY) {
+        console.log("not into yet")
+        $ele.css('align-items', 'flex-start');
+        $ele.css('position', 'static');
+    }
+    else if (windowScrollTop > elePosY + eleHeight - imgHeight) {
+        console.log("scroll complete")
+        $ele.css('align-items', 'flex-end');
+        $ele.css('position', 'static');
+    }
+    else {
+        console.log("change to fixed")
+        $ele.css('top', '0px');
+        $ele.css('width', eleWidth + 'px');
+        $ele.css('position', 'fixed');
+    }
+}
 
-// function instantiate(element, options) {
-//     var c = new Constellation(element, options);
-//     c.init();
-// }
 
-// $.fn.parallex = function (options) {
-//     return this.each(function () {
-
-//         instantiate(this, options);
-//     });
-// };
+$.fn.parallex = function () {
+    if (!window.parallex) window.parallex = new Parallex()
+    this.each(function () {
+        var parallexElement = new ParallexElement($(this))
+        parallex.elements.push(parallexElement)
+    });
+    window.parallex.onScroll()
+};
